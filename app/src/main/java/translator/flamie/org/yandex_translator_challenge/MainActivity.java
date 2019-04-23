dc83c9075246c20d77e2608b7c4ea4619e867f9e
Alex <rupta3@gmail.com>
2019-04-23 16:42:25 +0300

    Replaced conflicted deps

--- app/src/main/java/translator/flamie/org/yandex_translator_challenge/MainActivity.java
+++ app/src/main/java/translator/flamie/org/yandex_translator_challenge/MainActivity.java
@@ -3,12 +3,11 @@ package translator.flamie.org.yandex_translator_challenge;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentActivity;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
-
-import com.roughike.bottombar.BottomBar;
-import com.roughike.bottombar.OnTabSelectListener;
 
 import translator.flamie.org.yandex_translator_challenge.fragments.FavoritesFragment;
 import translator.flamie.org.yandex_translator_challenge.fragments.HistoryFragment;
@@ -34,12 +33,13 @@ public class MainActivity extends FragmentActivity {
         final FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.content_frame_layout);
         getFragmentManager().beginTransaction().add(contentFrameLayout.getId(), TranslatorFragment.newInstance(localData)).commit();
 
-        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
-        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+        BottomNavigationView bottomBar = (BottomNavigationView) findViewById(R.id.bottomBar);
+
+        bottomBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onTabSelected(@IdRes int tabId) {
-                System.out.println("Something");
-                System.out.println(tabId + " " + R.id.tab_translator);
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                final int tabId = menuItem.getItemId();
+
                 if (tabId == R.id.tab_translator) {
                     FragmentTransaction translatorTransaction = getFragmentManager().beginTransaction();
                     translatorTransaction.replace(R.id.content_frame_layout, TranslatorFragment.newInstance(localData));
@@ -54,17 +54,16 @@ public class MainActivity extends FragmentActivity {
                     historyTransaction.commit();
                     System.out.println("History");
                 }
-                if(tabId == R.id.tab_favorites) {
+                if (tabId == R.id.tab_favorites) {
                     FragmentTransaction historyTransaction = getFragmentManager().beginTransaction();
                     historyTransaction.replace(R.id.content_frame_layout, FavoritesFragment.newInstance(localData));
                     historyTransaction.addToBackStack(null);
                     historyTransaction.commit();
                     System.out.println("Favorites");
                 }
+
+                return true;
             }
         });
     }
-
-
-
 }
